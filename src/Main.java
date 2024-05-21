194981467e0b7fe0371854a0913ef06c72351639
Артем Капралов <58602723+Artem-Kapralov@users.noreply.github.com>
2024-05-22 02:01:37 +0300

    Добавлен ввод с клавиатуры, исправлен сброс настроек структуры проекта

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите систему счисления(2, 8, 10, 16): ");
+        String system = scanner.nextLine();
+        System.out.print("Введите первое число: ");
+        String a = scanner.nextLine();
+        System.out.print("Введите действие(+, -, *, /): ");
+        String deistv = scanner.nextLine();
+        System.out.print("Введите второе число: ");
+        String b = scanner.nextLine();
 
     }
 }
