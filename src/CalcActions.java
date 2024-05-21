4d1478e1384cf666459ff22dae4528e7cb5a2371
Артем Капралов <58602723+Artem-Kapralov@users.noreply.github.com>
2024-05-22 01:05:14 +0300

    Добавлен абстрактный класс с действиями

---
+++ src/CalcActions.java
@@ -0,0 +1,14 @@
+public abstract class CalcActions {
+    public static int Minus(int a, int b){
+        return a - b;
+    }
+    public static int Plus(int a, int b){
+        return a + b;
+    }
+    public static int Umn(int a, int b){
+        return a * b;
+    }
+    public static double Del(double a, double b){
+        return a / b;
+    }
+}
