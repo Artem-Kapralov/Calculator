bdd2a4f960bdd0d9abe7a527a5f09d6c17a38df1
Артем Капралов <58602723+Artem-Kapralov@users.noreply.github.com>
2024-05-22 08:46:32 +0300

    Лишний перенос

--- src/CalcActions.java
+++ src/CalcActions.java
@@ -2,9 +2,7 @@ public abstract class CalcActions {
     public static int Minus(int a, int b){
         return a - b;
     }
-    public static int Plus(int a, int b){
-        return a + b;
-    }
+    public static int Plus(int a, int b){ return a + b; }
     public static int Umn(int a, int b){
         return a * b;
     }
